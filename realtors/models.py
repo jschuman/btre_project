de223b28a039afc876f8c7ca2cab009f49f94f3f
Jeff Schuman <jschuman@solutionstreet.com>
2019-03-04 08:12:28 -0500

    fixed realtor model; regenerated migrations

--- realtors/models.py
+++ realtors/models.py
@@ -2,8 +2,8 @@ from django.db import models
 from datetime import datetime
 
 class Realtor(models.Model):
-    name: models.CharField(max_length=200)
-    photo: models.ImageField(upload_to='photos/%Y/%m/%d/')
+    name = models.CharField(max_length=200)
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
     description = models.TextField(blank=True)
     phone = models.CharField(max_length=20)
     email = models.CharField(max_length=50)
