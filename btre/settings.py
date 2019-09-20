18db06a8e448a3200c00225b36179ede781b35e0
Jeff Schuman <jschuman@solutionstreet.com>
2019-09-20 17:37:16 -0400

    Added local settings import

--- btre/settings.py
+++ btre/settings.py
@@ -148,3 +148,8 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = 'jschuman@gmail.com'
 EMAIL_HOST_PASSWORD = 'xmgthilpfgoqjtyo'
 EMAIL_USE_TLS = True
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
